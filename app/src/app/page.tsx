f52a039211c96e1fed579a69a2fc46dd18abbcab
minusno0708 <naoyao1014@gmail.com>
2024-02-23 02:03:58 +0900

    feat:時給の入力を追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import React, { useState } from "react";
 
 export default function Home() {
+  const [salary, setSalary] = useState("");
+
   return (
     <div>
       <h1>SALARY TIMER</h1>
       <div>
+        <input 
+          type="number" 
+          placeholder="時給"
+          value={salary}
+          onChange={(event) => setSalary(event.target.value)}
+        />
+        円
+      </div>
+      <div>
         <button>Start</button>
         <button>Stop</button>
         <button>Reset</button>
+      </div>
+      <div>
+        秒単価:{salary/(60*60)}円
       </div>
     </div>
   );
