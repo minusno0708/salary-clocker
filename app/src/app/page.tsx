b009fc3ae6ba5800b712441be16fae585f21c4d2
minusno0708 <naoyao1014@gmail.com>
2024-02-22 17:53:15 +0900

    feat/ボタンを設置

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
+
 export default function Home() {
   return (
     <div>
       <h1>SALARY TIMER</h1>
+      <div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
