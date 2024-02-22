3db59df5f3d86cd482b153de4a3b3337a32357fa
minusno0708 <naoyao1014@gmail.com>
2024-02-23 02:06:27 +0900

    refactor:お金に関する項目をまとめる

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,27 +3,25 @@
 import React, { useState } from "react";
 
 export default function Home() {
-  const [salary, setSalary] = useState("");
+  const [salary, setSalary] = useState(1000);
 
   return (
     <div>
       <h1>SALARY TIMER</h1>
       <div>
+        時給:
         <input 
           type="number" 
-          placeholder="時給"
           value={salary}
           onChange={(event) => setSalary(event.target.value)}
         />
         円
+        <p>秒単価:{salary/(60*60)}円</p>
       </div>
       <div>
         <button>Start</button>
         <button>Stop</button>
         <button>Reset</button>
-      </div>
-      <div>
-        秒単価:{salary/(60*60)}円
       </div>
     </div>
   );
